e7449d11c016a5d95317ecdd7f9b3731d48c6142
destruc123 <110302857+destruc123@users.noreply.github.com>
2023-07-02 12:24:47 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+// es para despedir
+console.log("este es la enterada ala pagina webs");
+// es para despedir
+console.log("adios");
+// esto es un comentario
+/*es tambien 
+es un comentario pero 
+varias lineas*/
